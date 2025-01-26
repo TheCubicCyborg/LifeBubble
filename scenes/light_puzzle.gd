eed8d7bdae04d2c1ff315a31f3a4ac1f081311c4
Lucas Zhong <zhonglucas@gmail.com>
2025-01-26 14:45:52 -0800

    light puzzle done

---
+++ scenes/light_puzzle.gd
@@ -0,0 +1,69 @@
+extends Control
+
+@export var buttons : Array[TextureRect]
+@export var bulbs : Array[TextureRect]
+
+@export var unlit_texture : Texture2D
+@export var lit_texture : Texture2D
+
+@export var animation_player : AnimationPlayer
+
+var bulb_states = [false, false, false]
+var button_index := 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	button_index = 0
+	_set_selected_button(button_index)
+	animation_player.play('RESET')
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("interact"):
+		activate(button_index)
+	elif Input.is_action_just_pressed("move_down"):
+		if button_index < 2:
+			button_index += 1
+			_set_selected_button(button_index)
+	elif Input.is_action_just_pressed("move_up"):
+		if button_index > 0:
+			button_index -= 1
+			_set_selected_button(button_index)
+	
+	if check_win():
+		await get_tree().create_timer(1.0).timeout
+		animation_player.play('FlyOut')
+		await animation_player.animation_finished
+		queue_free()
+
+func check_win() -> bool:
+	for bulb_state in bulb_states:
+		if bulb_state == false:
+			return false
+	return true	
+			
+func activate(button_index: int) -> void:
+	if button_index == 0:
+		change_bulb_state(0)
+		change_bulb_state(2)
+	elif button_index == 1:
+		change_bulb_state(0)
+	elif button_index == 2:
+		change_bulb_state(0)
+		change_bulb_state(1) 
+	
+func change_bulb_state(bulb_index: int) -> void:
+	bulb_states[bulb_index] = not bulb_states[bulb_index]
+	for i in range(3):
+		if bulb_states[i] == true:
+			bulbs[i].texture = lit_texture
+		else:
+			bulbs[i].texture = unlit_texture
+
+func _set_selected_button(index) -> void:
+	for button in buttons:
+		var mat : ShaderMaterial = button.material
+		mat.set_shader_parameter("width", 0)
+
+	var mat : ShaderMaterial = buttons[index].material
+	mat.set_shader_parameter("width", 1)
