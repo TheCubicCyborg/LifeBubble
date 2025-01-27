d401cdc0390a9ab708728e500e406c4b1a11296c
Lucas Zhong <zhonglucas@gmail.com>
2025-01-26 16:55:09 -0800

    inventory fixes

--- scenes/InventoryManager.gd
+++ scenes/InventoryManager.gd
@@ -18,8 +18,18 @@ var accepting_input:bool
 @onready var mainInv = $MainInv
 @onready var notesList = $NotesList
 
+@export var notes : Array[TextureRect]
+@export var note_textures : Array[Texture]
+@export var blank_texture : Texture
+
+var selected_note := 0
+
+var in_notes := false
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	in_notes = false
+	selected_note = 0
 	set_inv_visibility(false)
 	for key in item_textures:
 		get_node(item_textures[key]).visible = false
@@ -36,6 +46,25 @@ func _process(delta):
 	elif accepting_input and Input.is_action_just_pressed("inventory"):
 		close_inventory()
 	if accepting_input:
+		var note_count = num_of_notes()
+		if note_count > 0:
+			%FocusPos.visible = true
+		elif note_count > 1:
+			%RightPos.visible = true
+		elif note_count > 2:
+			%LeftPos.visible = true
+		if in_notes:
+			if Input.is_action_just_pressed("escape"):
+				exit_notes()
+				return
+			elif Input.is_action_just_pressed("interact"):
+				pull_up_note(selected_note)
+			elif Input.is_action_just_pressed("move_left"):
+				selected_note = wrapi(selected_note + 1, 0, 5)
+			elif Input.is_action_just_pressed("move_right"):
+				selected_note = wrapi(selected_note - 1, 0, 5)
+			display_notes()
+			return
 		var inputted = false
 		if Input.is_action_just_pressed("move_up") and selected.y != 0: 
 			selected.y -= 1
@@ -44,7 +73,7 @@ func _process(delta):
 			selected.x += 1
 			selected.y = 0
 			inputted = true
-		elif Input.is_action_just_pressed("move_down") and selected.y != len(item_moves[selected.x]) -1 : 
+		elif Input.is_action_just_pressed("move_down") and selected.y != len(item_moves[selected.x]) -1 and num_of_notes() > 0: 
 			selected.y += 1
 			inputted = true
 		elif Input.is_action_just_pressed("move_left") and selected.x != 0 : 
@@ -55,9 +84,24 @@ func _process(delta):
 			print(arr_vect2(item_moves, selected))
 			set_outline(arr_vect2(item_moves, selected))
 		else:
-			if Input.is_action_just_pressed("interact") and arr_vect2(item_moves, selected) == 'notes':
+			if Input.is_action_just_pressed("interact") and arr_vect2(item_moves, selected) == 'notes' and num_of_notes() > 0:
 				bring_up_notes()
 
+func pull_up_note(note_index: int) -> void:
+	print("SELECT: ", selected_note)
+
+func display_notes():
+	%FocusPos.texture = note_textures[selected_note]
+	if wrapi(selected_note - 1, 0, 5) in Globals.GameManager.notes_obtained:
+		%LeftPos.texture = note_textures[wrapi(selected_note - 1, 0, 5)]
+	else:
+		%LeftPos.texture = blank_texture
+	
+	if wrapi(selected_note + 1, 0, 5) in Globals.GameManager.notes_obtained:
+		%RightPos.texture = note_textures[wrapi(selected_note + 1, 0, 5)]
+	else:
+		%RightPos.texture = blank_texture
+ 	
 func open_inventory():
 	set_inv_visibility(true)
 	Globals.Player.accepting_input = false
@@ -88,7 +132,15 @@ func populate_found():
 
 func bring_up_notes():
 	animation_player.play('notes_in')
+	in_notes = true
+	
+func exit_notes():
+	animation_player.play('notes_out')
+	in_notes = false
 
 func pickup(item:String):
 	found[item] = true
 	get_node(item_textures[item]).visible = true
+
+func num_of_notes() -> int:
+	return len(Globals.GameManager.notes_obtained)
